ede03aeaee1db6890b68954409c01f4781b3b605
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-06-19 18:11:58 +0200

    Feature : implémentation de la page ShowClientsList

---
+++ src/pages/ShowClientsList.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type Client = {
+  Client: string;
+  Adresse: string;
+  Téléphone: string;
+};
+
+const getClientsList = (): Client[] => {
+  const data = localStorage.getItem("clientsList");
+  return data ? JSON.parse(data) : [];
+};
+
+const clients = getClientsList();
+
+const ShowClientsList: React.FC = () => {
+  return (
+    <div>
+      <h1>Liste des clients</h1>
+      <div className="card-container">
+        {clients.map((client, idx) => (
+          <div className="card" key={idx}>
+            <h2>{client.Client}</h2>
+            <p>{client.Adresse}</p>
+            <p>{client.Téléphone}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ShowClientsList;

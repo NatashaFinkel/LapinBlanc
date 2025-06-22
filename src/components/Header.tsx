3c352372ca39d8e8596496c6190ef2d4a7a96467
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-06-22 17:40:12 +0200

    Feature : implémentation du Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/tous les clients">
+              Tous les clients
+            </a>
+          </li>
+          <li>
+            <a href="/nouveau-client">Ajouter un client</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
